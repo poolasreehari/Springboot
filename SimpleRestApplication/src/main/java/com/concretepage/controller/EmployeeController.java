1f65e6ac250ba8070bbca3a715efa03ce5f01b4e
sreehari <sreehari@192.168.0.7>
2018-06-20 22:24:06 +0530

    simple rest restservice using spring boot

---
+++ SimpleRestApplication/src/main/java/com/concretepage/controller/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.concretepage.controller;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.concretepage.domain.Employee;
+import com.concretepage.service.EmployeeService;
+@RestController
+public class EmployeeController {
+	@Autowired
+	private EmployeeService service;
+	@RequestMapping("home")
+	public List<Employee> showData() {
+		List<Employee> list = service.getAllEmployee();
+		return list;
+ 	}
+} 
