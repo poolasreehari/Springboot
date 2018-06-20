7ba47819bd708fd38cebe03ff68cd12f10e3e9f2
sreehari <sreehari@192.168.0.7>
2018-06-20 22:08:02 +0530

    new change in spring boot class

--- SimpleRestApplication/src/main/java/com/concretepage/SimpleRestApplication.java
+++ SimpleRestApplication/src/main/java/com/concretepage/SimpleRestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleRestApplication {
 	public static void main(String[] args) {
+		System.out.println("poola");
 		SpringApplication.run(SimpleRestApplication.class, args);
 	}
 }
